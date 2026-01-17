b81c31372a08294da76614ea3b4c58a7965bb0dd
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-01-17 23:34:32 +0530

    User fixed

---
+++ src/main/java/com/yashhh/Backend_MP/Entity/User.java
@@ -0,0 +1,25 @@
+package com.yashhh.Backend_MP.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
