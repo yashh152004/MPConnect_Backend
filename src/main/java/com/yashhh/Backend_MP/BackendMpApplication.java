3703a08f27121327f092826d6ac102eb872c3ae0
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-01-17 23:17:28 +0530

    Folder setup

---
+++ src/main/java/com/yashhh/Backend_MP/BackendMpApplication.java
@@ -0,0 +1,13 @@
+package com.yashhh.Backend_MP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMpApplication.class, args);
+	}
+
+}
