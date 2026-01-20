0c02336445c058f6fe8dcbedac3f1e7c0c953e9b
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-01-20 20:05:44 +0530

    Repo for Complaints added

---
+++ src/main/java/com/yashhh/Backend_MP/Repository/ComplaintEvidenceRepository.java
@@ -0,0 +1,8 @@
+package com.yashhh.Backend_MP.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yashhh.Backend_MP.Entity.ComplaintEvidence;
+
+public interface ComplaintEvidenceRepository extends JpaRepository<ComplaintEvidence, Long> {
+}
---
+++ src/main/java/com/yashhh/Backend_MP/Repository/ComplaintRepository.java
@@ -0,0 +1,11 @@
+package com.yashhh.Backend_MP.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yashhh.Backend_MP.Entity.Complaint;
+
+public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+    List<Complaint> findByCreatedById(Long userId);
+}
