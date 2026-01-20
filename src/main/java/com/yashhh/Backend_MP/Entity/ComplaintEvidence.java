83bf1ded1f52df99d1750725dcc97a703480f4b6
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-01-20 20:03:29 +0530

    Complaint_Portal added

---
+++ src/main/java/com/yashhh/Backend_MP/Entity/ComplaintEvidence.java
@@ -0,0 +1,34 @@
+package com.yashhh.Backend_MP.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "complaint_evidence")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ComplaintEvidence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+    private String fileType;
+    private String filePath;
+
+    @ManyToOne
+    @JoinColumn(name = "complaint_id")
+    private Complaint complaint;
+}
