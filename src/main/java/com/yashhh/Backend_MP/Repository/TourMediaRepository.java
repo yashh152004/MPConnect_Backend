bd1916ec4f26d492ac6f5499782e3c266ef55553
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-01-23 17:30:45 +0530

    Repo for tour_media added

---
+++ src/main/java/com/yashhh/Backend_MP/Repository/TourMediaRepository.java
@@ -0,0 +1,8 @@
+package com.yashhh.Backend_MP.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yashhh.Backend_MP.Entity.TourMedia;
+
+public interface TourMediaRepository extends JpaRepository<TourMedia, Long> {
+}
